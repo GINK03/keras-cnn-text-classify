0d0141ddbb2cdc263e482f5aeaa6a1bff8a84b09
Your Name <gim.kobayashi@gmail.com>
2017-12-01 21:33:06 +0900

    a

---
+++ keras2-text-tag-predictor/cnn_text_classify.py
@@ -0,0 +1,91 @@
+import pickle
+import gzip
+import numpy as np
+import random
+import os
+import sys
+import statistics
+import glob
+import re
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential, Model, load_model
+from keras.layers import Lambda, Input, Activation, Dropout, Flatten, Dense, Reshape, merge
+from keras.layers import Concatenate, Multiply, Conv1D, MaxPool1D, BatchNormalization
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers.normalization import BatchNormalization as BN
+from keras.layers.core import Dropout
+from keras.optimizers import SGD, Adam
+from keras import backend as K
+
+def CBRD(inputs, filters=64, kernel_size=3, droprate=0.5):
+  x = Conv1D(filters, kernel_size, padding='same',
+            kernel_initializer='random_normal')(inputs)
+  x = BatchNormalization()(x)
+  x = Activation('relu')(x)
+  return x
+
+
+def DBRD(inputs, units=4096, droprate=0.35):
+  x = Dense(units)(inputs)
+  x = BatchNormalization()(x)
+  x = Activation('relu')(x)
+  x = Dropout(droprate)(x)
+  return x
+
+input_tensor = Input( shape=(200, 4954) )
+
+x = Dense(3000, activation='relu')(input_tensor)
+x = CBRD(x, 16)
+x = CBRD(x, 16)
+x = MaxPool1D()(x)
+
+x = CBRD(x, 32)
+x = CBRD(x, 32)
+x = MaxPool1D()(x)
+
+x = CBRD(x, 64)
+x = CBRD(x, 64)
+x = MaxPool1D()(x)
+
+x = CBRD(x, 128)
+x = CBRD(x, 128)
+x = CBRD(x, 128)
+x = MaxPool1D()(x)
+
+x = CBRD(x, 128)
+x = CBRD(x, 128)
+x = CBRD(x, 128)
+x = MaxPool1D()(x)
+
+x = Flatten()(x)
+print( x.shape )
+x = Dense(10000, name='a', activation='sigmoid')(x)
+print( x.shape )
+model = Model(inputs=input_tensor, outputs=x)
+model.compile(loss='mae', optimizer='sgd')
+
+if '--train' in sys.argv:
+  init = 0
+  try:
+    target_model = sorted(glob.glob('models/*.h5'))[-1]
+    model.load_weights( target_model ) 
+    init = int( re.search(r'/(\d{1,}).h5', target_model).group(1) )
+    print('init state update', init)
+  except Exception as e:
+    print(e)
+    ...
+  for i in range(init, 5000):
+    files = glob.glob('pairs/*.pkl.gz')
+    ys, Xs = [], []
+    for name in random.sample(files, 200):
+      try:
+        X, y = pickle.loads( gzip.decompress( open(name, 'rb').read() ) )
+      except EOFError as e:
+        continue
+      #print( X.shape )
+      ys.append(y)
+      Xs.append(X)
+    ys,Xs = np.array(ys), np.array(Xs)
+    model.fit(Xs, ys, epochs=10, batch_size=64)
+    model.save_weights('models/{:09d}.h5'.format(i))
---
+++ keras2-text-tag-predictor/prepare.py
@@ -0,0 +1,107 @@
+import os
+import sys
+
+import pickle
+import gzip
+import random
+import concurrent.futures 
+import json
+import zipfile
+import numpy as np
+if '--tag_index' in sys.argv:
+  tag_freq = {}
+  z = zipfile.ZipFile('tag_introduction.zip')
+
+  for name in z.namelist():
+    f = z.open(name)
+    raw = f.read().decode('utf8') 
+    try:
+      obj = json.loads(raw)
+    except:
+      continue
+    for tag in obj['tags']:
+      if tag_freq.get(tag) is None:
+        tag_freq[tag] = 0
+      tag_freq[tag] += 1
+
+  tag_index = {} 
+  for index, (tag, freq) in enumerate( random.sample(sorted(tag_freq.items(), key=lambda x:x[1]*-1)[:10000], k=10000)):
+    print( index, tag, freq )
+    tag_index[tag] = index
+  open('tag_index.json', 'w').write( json.dumps(tag_index, indent=2, ensure_ascii=False) )
+
+if '--char_index' in sys.argv:
+  tag_freq = {}
+  z = zipfile.ZipFile('tag_introduction.zip')
+  chars = set()
+  for name in z.namelist():
+    f = z.open(name)
+    raw = f.read().decode('utf8') 
+    try:
+      obj = json.loads(raw)
+    except:
+      continue
+    for char in list(obj['intro']):
+      chars.add(char)
+
+  char_index = {char:index for index, char in enumerate(chars)}
+  open('char_index.json', 'w').write( json.dumps(char_index, indent=2, ensure_ascii=False) )
+
+def _map2(arr):
+  fi, line = arr
+
+  if os.path.exists('pairs/{:09d}.pkl.gz'.format(fi)) is True:
+    return "Already processed."
+  try:
+    star, text = line.split(' __SEP__ ')
+  except ValueError as e:
+    return 
+  
+  base = [ [0.0]*size for i in range(width) ]
+  for index, ch in enumerate( list(text) ):
+    #print( char_index[ch] )
+    try:
+      base[index][ char_index[ch] ] = 1.0
+    except Exception as e:
+      break
+
+  star = float(star)
+  base = np.array(base)
+  print('now iter', fi)
+  open('pairs/{:09d}.pkl.gz'.format(fi), 'wb').write( gzip.compress(pickle.dumps( (star, base) ) ) ) 
+
+if '--make_pair' in sys.argv:
+  char_index = json.loads( open('char_index.json').read()  )
+  tag_index = json.loads( open('tag_index.json').read()  )
+  char_size = len(char_index)
+  tag_size = len(tag_index)
+  
+  z = zipfile.ZipFile('tag_introduction.zip')
+  chars = set()
+  for name in z.namelist():
+    
+    f = z.open(name)
+    raw = f.read().decode('utf8') 
+    save_name = 'pairs/{}.pkl.gz'.format(name.split('/').pop())
+    if os.path.exists(save_name) is True:
+      continue
+    try:
+      obj = json.loads(raw)
+    except:
+      continue
+    y = [tag_index[tag] for tag in obj['tags'] if tag_index.get(tag) is not None]
+    X = [char_index[char] for char in obj['intro'] ]
+    
+    baseX = [ [0.0]*char_size for i in range(200) ]
+    for index, Xi in enumerate(X[:200]):
+      baseX[index][Xi] = 1.0
+
+    baseY = [0.0]*tag_size  
+    for index, Yi in enumerate(y[:50]):
+      baseY[Yi] = 1.0 
+
+    try:
+      open(save_name, 'wb').write( gzip.compress(pickle.dumps( ( baseX, baseY ) ) ) )
+    except OSError as ex:
+      continue
+    print( name )
